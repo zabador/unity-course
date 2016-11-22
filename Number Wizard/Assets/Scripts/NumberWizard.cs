6ae98b2abfc5b3a0827c82a40041ac7f218e7fdb
schneis <skye.schneider@dominos.com>
2016-11-22 09:29:24 -0500

    Update main script

--- Number Wizard/Assets/Scripts/NumberWizard.cs
+++ Number Wizard/Assets/Scripts/NumberWizard.cs
@@ -3,13 +3,15 @@ using System.Collections;
 
 public class NumberWizard : MonoBehaviour {
 
+    int min = 1;
+    int max = 1000;
+
     // Use this for initialization
     void Start() {
         print("Welcome to number wizard");
         print("Pick a number in your head but don't tell me");
 
-        int min = 1;
-        int max = 1000;
+
 
         print("The highest number you can pick is " + max);
         print("The lowest number you can pick is " + min);
@@ -25,8 +27,7 @@ public class NumberWizard : MonoBehaviour {
         } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
             print("down arrow pressed");
         } else if (Input.GetKeyDown(KeyCode.Return)) {
-            print("Return key pressed");
+            print("I won");
         }
-	
     }
 }
