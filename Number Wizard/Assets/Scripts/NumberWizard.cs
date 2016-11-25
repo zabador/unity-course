0b589d4d72e5646f593c6e924bd10043c171f138
zabador <zabador2000@gmail.com>
2016-11-25 13:28:46 -0500

    Refactor number wizard

--- Number Wizard/Assets/Scripts/NumberWizard.cs
+++ Number Wizard/Assets/Scripts/NumberWizard.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class NumberWizard : MonoBehaviour {
 
-    int min = 1;
-    int max = 1000;
+    int min;
+    int max;
     int guess;
 
     // Use this for initialization
     void Start() {
-        
+        startGame(); 
+       
+    }
+
+    void startGame() {
+        min = 1;
+        max = 1000;
         print("Welcome to number wizard");
         print("Pick a number in your head but don't tell me");
 
@@ -18,24 +24,28 @@ public class NumberWizard : MonoBehaviour {
         print("The highest number you can pick is " + max);
         print("The lowest number you can pick is " + min);
         guess = max / 2;
-        max = max + 1;
+        max++;
 
         print("Is your number higher or lower that " + guess);
         print("Enter 'Up' for higher 'down' for lower and 'enter' for equals");
+    }
+
+    void nextGuess() {
+        guess = (max + min) / 2;
+        print("Is the number higher or lower than " + guess); 
     }
 	
     // Update is called once per frame
     void Update() {
         if (Input.GetKeyDown(KeyCode.UpArrow)) {
             min = guess;
-            guess = (max + min) / 2;
-            print("Is the number higher or lower than " + guess);
+            nextGuess();
         } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
             max = guess;
-            guess = (max + min) / 2;
-            print("Is the number higher or lower than " + guess);
+            nextGuess();
         } else if (Input.GetKeyDown(KeyCode.Return)) {
             print("I won");
+            startGame();
         }
     }
 }
