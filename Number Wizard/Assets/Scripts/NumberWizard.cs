69bccdc90054101e427daa08c6ca3060fb97b2c2
schneis <skye.schneider@dominos.com>
2016-11-25 13:21:17 -0500

    Finish first number wizard

--- Number Wizard/Assets/Scripts/NumberWizard.cs
+++ Number Wizard/Assets/Scripts/NumberWizard.cs
@@ -5,9 +5,11 @@ public class NumberWizard : MonoBehaviour {
 
     int min = 1;
     int max = 1000;
+    int guess;
 
     // Use this for initialization
     void Start() {
+        
         print("Welcome to number wizard");
         print("Pick a number in your head but don't tell me");
 
@@ -15,17 +17,23 @@ public class NumberWizard : MonoBehaviour {
 
         print("The highest number you can pick is " + max);
         print("The lowest number you can pick is " + min);
+        guess = max / 2;
+        max = max + 1;
 
-        print("Is your number higher or lower that " + max / 2);
+        print("Is your number higher or lower that " + guess);
         print("Enter 'Up' for higher 'down' for lower and 'enter' for equals");
     }
 	
     // Update is called once per frame
     void Update() {
         if (Input.GetKeyDown(KeyCode.UpArrow)) {
-            print("Up arrow pressed");
+            min = guess;
+            guess = (max + min) / 2;
+            print("Is the number higher or lower than " + guess);
         } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
-            print("down arrow pressed");
+            max = guess;
+            guess = (max + min) / 2;
+            print("Is the number higher or lower than " + guess);
         } else if (Input.GetKeyDown(KeyCode.Return)) {
             print("I won");
         }
